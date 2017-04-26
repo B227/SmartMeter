e0fc0e0a0794c94fb734dae1461b750f785bf3f2
RolfKro <rkraye16@student.aau.dk>
2017-04-26 12:32:19 +0200

    real life with sim data
    complete with real life voltage data from sim in flash memory and sinus
    function for amp, so new .h file with big array and a bit of house
    cleaning

--- emulator/emulator.ino
+++ emulator/emulator.ino
@@ -1,23 +1,26 @@
+#include <avr/pgmspace.h>
+#include "voltage.h"
+
 unsigned long time = millis();
-//Sets the sample rate in ms
+//Sets the sample rate in ms, lowest is 5 and other set functions are a multiple of 5 seconds
 long Sample_Speed = 1;
 
-//Set sim speed
+//Set sim speed, 1 is real time
 long Sim_Speed = 1;
+//samp 120 and sim 600 is a good rate for quick test
 
-//sete max and min
-int Max=401;
-int Min=395;
+//set max and min
+int Max = 5;
+int Min = 1;
+
+int index = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  //while (!Serial) {
-  //  ; // wait for serial port to connect. Needed for native USB port only
-  //}
 
   //Random seed for true random
-  randomSeed(analogRead(0));
+  //randomSeed(analogRead(0));
 
   Serial.print("Sample rate at: ");
   Serial.println(Sample_Speed);
@@ -44,22 +47,47 @@ struct data_types {
 data_types emu_data;
 
 struct data_types emulator(int sim, int sample) {
-  if (time + ((Sample_Speed * 1000) / sim) <= millis()) {
+  float volt_temp;
+  float amp_temp;
+  float effect_temp;
+  if (index == 8640) {
+    index = 0;
+  }
+  if (time + ((Sample_Speed * 5000) / sim) <= millis()) {
     data_types temp;
+
+    //real life time stamps not affected by sim speed
     Serial.print("Sample at: ");
-    Serial.println(time);
+    Serial.println(time / 1000);
+
+    //save time
     temp.Time_Stamp = (time * sim);
-    temp.Voltage = (sin(((temp.Time_Stamp/1000)*3.14159)/43200)*((Max-Min)/2)+((Max+Min)/2))*1000;
-     Serial.println(temp.Voltage);
-    //Serial.println(temp.Voltage);
-    temp.Ampere = random(1, 1);
-    //Serial.println(temp.Ampere);
-    temp.Effect = temp.Ampere * temp.Voltage;
-    temp.Effect_Hour = (temp.Effect / (3.6 / sample));
+
+    //12 hours cycle
+    temp.Voltage = pgm_read_word_near(volt_array + index);
+    index++;
+    volt_temp=temp.Voltage;
+    //10 times bigger
+    //Serial.println(volt_temp/10,1);
+
+    //12 hours cycle
+    temp.Ampere = ((sin(((temp.Time_Stamp / 1000) * 3.14159) / 21600) * ((Max - Min) / 2)) + ((Max + Min) / 2)) * 1000;
+    amp_temp=temp.Ampere;
+    //1000 times bigger
+    //Serial.println(amp_temp/1000,3);
+
+    //effects
+    temp.Effect = (amp_temp * volt_temp) * 5;
+    temp.Effect_Hour = (temp.Effect / ((3600) / sample));
+    //10000 bigger than what it really is
+    effect_temp = temp.Effect;
+    effect_temp = (effect_temp / ((3600) / sample)) / 10000;
+
+    //info print
     Serial.print("Emulated effect: ");
-    Serial.print(temp.Effect_Hour);
+    Serial.print(effect_temp, 3);
     Serial.print(" Wh at :");
-    Serial.println(temp.Time_Stamp);
+    Serial.println(temp.Time_Stamp / 1000);
     time = millis();
     return temp;
   }
